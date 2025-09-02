6e563fe1ef23f9447e997d63a53e0aa4f4e314ec
juhyuk-1225 <hongjuhyuk@kakao.com>
2025-09-02 19:44:23 +0900

    Update score.py

--- average_score/score.py
+++ average_score/score.py
@@ -2,6 +2,7 @@
 import sys
 import csv
 
+filepath = "D:/주혁/PythonProject/text/score.csv"
 
 def load_from_csv(filepath):
     """
@@ -21,21 +22,48 @@ def load_from_csv(filepath):
             student_scores[row[0]] = row[1:]
     return student_scores, header[1:]
 
+student_scores, subjects = load_from_csv(filepath)
+
+
 
 def subject_average(student_scores: dict, subjects: list):
     """
     이 반의 각 과목별 평균을 구해서 딕셔너리로 반환
     예) {"국어": 80.8, "수학": 35.3, "영어": 96.6, "과학": 85.3, "사회": 38.8}
     """
-    pass
+
+    sub_avg = {}
+    for j in range(len(subjects)):
+        sum = 0
+        for i in student_scores.values():
+            sum += int(i[j])
+
+        sub_avg[subjects[j]] = float(f'{sum/len(student_scores.keys()):.1f}')
+
+    return sub_avg
+
+s = subject_average(student_scores, subjects)
+print(f'{s}')
 
 
-def student_average(student_scores: dict):
+
+def student_average(student_scores: dict, subjects: list):
     """
     각 학생별 전과목 평균 점수를 정렬된 튜플의 리스트로 반환
     예) [("이영희", 89.8), ("김철수", 86.6), ("박민수", 84.8)]
     """
-    pass
+    stu_ave = []
+    for k, l in student_scores.items():
+        stu_sum = 0
+        for m in l:
+            stu_sum += int(m)
+            ave = float(f'{stu_sum/len(subjects):.1f}')
+        stu_ave.append((k, ave))
+    return sorted(stu_ave, key=lambda x: x[1])
+sa = student_average(student_scores, subjects)
+print(sa)
+
+
 
 
 if __name__ == "__main__":
@@ -45,7 +73,7 @@ if __name__ == "__main__":
 
     student_scores, subjects = load_from_csv(sys.argv[1])
     sub_avg = subject_average(student_scores, subjects)
-    stud_avg = student_average(student_scores)
+    stud_avg = student_average(student_scores, subjects)
 
     print("과목 평균:")
     for sub, avg in sub_avg.items():
